6818d5ae3e812ab11a290214213d304731eb88d1
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-08 19:18:46 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -5,20 +5,55 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-              
+            arrayEmail: []
         }
     },
     methods: {
+        callApi() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((rispostaApi) => {
+                console.log(rispostaApi.data.response);
+                risposta = rispostaApi.data.response;
+                this.arrayEmail.push(risposta);
+            })
+        },
+
+        pulisciArray() {
+            this.arrayEmail=[]
+            console.log("cancella"+this.arrayEmail);
+            for (let i = 0; i < 10; i++) {
+                this.callApi();
+            }
+        },
+        
+    },
+
+    computed: {
 
     },
-    computed:{
+
+    created() {
+        console.log("created")
+        for (let i = 0; i < 10; i++) {
+            this.callApi();
+        }
+
 
     },
-    
+
     mounted() {
-       console.log();
+        console.log("mounted");
+    },
+    beforeCreated() {
+        console.log("beforeCreated")
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate")
+    },
+    update() {
+        console.log("Update")
     }
 
-    });
+});
 app.mount('#app')
+
 
