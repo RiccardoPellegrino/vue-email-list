15e81991918cb4e65017aa19f692e1f93ff03c79
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-08 14:31:54 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use-strict"
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+              
+        }
+    },
+    methods: {
+
+    },
+    computed:{
+
+    },
+    
+    mounted() {
+       console.log();
+    }
+
+    });
+app.mount('#app')
+
